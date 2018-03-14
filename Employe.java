ddd6997445a4149f0db03cde143f32bbd9703170
Bilal Fall <bilxadim@gmail.com>
2018-03-14 19:44:52 +0000

    Ajout des fichiers Employe et Etudiant

---
+++ Employe.java
@@ -0,0 +1,17 @@
+public class Employe{
+	//Proprietes
+	private String nomEmploye;
+	public String prenomEmploye;
+	
+	//Constructeur
+	
+	public Employe(){
+		
+	}
+	
+	public Employe(String nom, String prenom){
+		this.nomEmploye=nom;
+		
+		this.prenomEmploye=prenom;
+	}
+}
---
+++ Etudiant.java
@@ -0,0 +1,17 @@
+public class Etudiant{
+	//Proprietes
+	private String nomEtudiant;
+	public String prenomEtudiant;
+	
+	//Constructeur
+	
+	public Etudiant(){
+		
+	}
+	
+	public Etudiant(String nom, String prenom){
+		this.nomEtudiant=nom;
+		
+		this.prenomEtudiant=prenom;
+	}
+}
